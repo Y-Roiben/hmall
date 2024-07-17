93599d7443bda3a565dbfaa1afd256cf953563a7
Roiben <roiben@163.com>
2024-07-17 17:35:23 +0800

    init

---
+++ hm-service/src/test/java/com/hmall/service/impl/ItemServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.hmall.service.impl;
+
+import com.hmall.domain.dto.OrderDetailDTO;
+import com.hmall.service.IItemService;
+import com.hmall.utils.JwtTool;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+import java.util.List;
+
+@SpringBootTest
+class ItemServiceImplTest {
+
+
+}
